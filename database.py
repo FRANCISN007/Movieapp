2e154da51928aa7708d35f96e38443421b387d0c
unknown <francisnujimem@gmail.com>
2024-08-15 23:50:28 +0100

    update

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 #database.py
+from sqlalchemy import create_engine
 import os
 from dotenv import load_dotenv
-from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
